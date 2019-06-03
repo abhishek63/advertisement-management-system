f600a7ef58d0f6d753e7f5a231d455fc3ee98fa0
Abhishek Kumar <kumargolu63@gmail.com>
2019-06-03 21:48:19 +0530

    User, Post and Home Component Created

---
+++ src/components/posts-list/Category.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Category extends Component {
+    render() {
+        return (
+            <div>
+                Serch Bar
+            </div>
+        )
+    }
+}
+
+export default Category
---
+++ src/components/user/ViewAd.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ViewAd extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ViewAd
