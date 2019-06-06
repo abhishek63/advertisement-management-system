a5d90616047d8211bd627bba8a0a0cff19a306e3
Abhishek Kumar <kumargolu63@gmail.com>
2019-06-06 09:51:32 +0530

    check only

---
+++ src/components/user/Rohan.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Rohan extends Component {
+    render() {
+        return (
+            <div>
+                tohhhhh
+            </div>
+        )
+    }
+}
+
+export default Rohan
